b7162131850a19ac54c6714f5732b6b8d5781a5f
Hardik Bindal <123260152+hardikbindall@users.noreply.github.com>
2024-11-12 12:30:19 +0530

    commit

--- dom-ex.js
+++ dom-ex.js
@@ -1,8 +1,8 @@
 const btnl=document.getElementById("addpara")
-btnl.addEventListener("click",()=>){
+btnl.addEventListener("click",()=>{
     const para=document.createElement("p");
     para.innerHTML="<h1>This is a new Paragraph";
     para.style.color="white";
     para.style.backgroundColor="black";
     document.body.append(para);
-}
+})
