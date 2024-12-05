32a2b19442f7cef1601111b688387c4211ce46fe
Hardik Bindal <123260152+hardikbindall@users.noreply.github.com>
2024-12-05 15:33:39 +0530

    commit

--- JS/Data.js
+++
@@ -1,9 +0,0 @@
-const qBank={
-    id: 1,
-    ques: "Full Form of JSP?",
-    opt1: "Java Server Pages",
-    opt2: "Java Server Print",
-    opt3: "Java Site Pages",
-    opt4: "Java Static Pages",
-    ans: "Java Server Pages",
-}
--- JS/dom-ex.js
+++ JS/dom-ex.js
@@ -1,8 +1,8 @@
-const btnl=document.getElementById("addpara")
-btnl.addEventListener("click",()=>{
+const btn1=document.getElementById('addpara');
+btn1.addEventListener("click",()=>{
     const para=document.createElement("p");
-    para.innerHTML="<h1>This is a new Paragraph";
-    para.style.color="white";
-    para.style.backgroundColor="black";
+    para.innerHTML="<h1>this is new paragraph</h1>";
+    para.style.color='black';
     document.body.append(para);
+    para.style.backgroundColor='#765857'
 })
