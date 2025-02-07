12fcf2604af98c3254aab87564e9eaddc544d19d
Hardik Bindal <123260152+hardikbindall@users.noreply.github.com>
2025-02-07 13:05:19 +0530

    commit

---
+++ Backend/Practice_Node/7feb-1.js
@@ -0,0 +1,6 @@
+const sum=require('./usemodule');
+const result= sum(12,20);
+console.log("sum"+result);
+
+const result= multiplication(12,20);
+console.log("Multiplication"+result);
---
+++ Backend/Practice_Node/7feb-2.js
@@ -0,0 +1,3 @@
+const fs=require('fs');
+fs.writeFileSync("data.txt","Hello Writing data using node js")
+console.log("Data Written Successfully!!");
